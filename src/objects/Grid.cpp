466a5f8f46596e622e1ef316a80dbe2dcaaabd34
DagnisRR <drremesis@gmail.com>
2024-03-02 23:15:07 +0200

    [ v0.1.3.0 ] Program restructuring - grid added to scene

---
+++ src/objects/Grid.cpp
@@ -0,0 +1,36 @@
+#include <Magnum/Math/Color.h>
+#include <Magnum/Math/Vector.h>
+#include <Magnum/Math/Matrix4.h>
+
+#include <Magnum/Trade/MeshData.h>
+#include <Magnum/Primitives/Grid.h>
+
+#include <Magnum/MeshTools/Compile.h>
+#include <Magnum/Meshtools/Copy.h>
+#include <Magnum/MeshTools/Transform.h>
+
+#include "Grid.h"
+#include "../scene/Scene.h"
+
+namespace Magnum
+{
+    using namespace Math::Literals;
+
+    Grid::Grid(
+        Scene3D &scene,
+        SceneGraph::DrawableGroup3D &drawables,
+        float size,
+        Vector2i subdivisions,
+        Color3 color) : _scene(scene), _drawables(drawables)
+    {
+        const Trade::MeshData _meshData = Primitives::grid3DWireframe(subdivisions);
+        Trade::MeshData _mutableMeshData = MeshTools::copy(_meshData);
+        MeshTools::transformPointsInPlace(Matrix4::scaling(Vector3{size}) * Matrix4::rotationX(90.0_degf),
+                                          _mutableMeshData.mutableAttribute<Vector3>(Trade::MeshAttribute::Position));
+        _mesh = MeshTools::compile(_mutableMeshData);
+        _shader = Shaders::FlatGL3D{};
+        _shader.setColor(color);
+        auto object = new Object3D{&_scene};
+        new FlatGLDrawable{*object, _shader, _mesh, _drawables};
+    }
+}
